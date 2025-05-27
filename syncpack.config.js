8fbcd4f27251f1f2400e5a61070eab120a0f3735
Mark <m4rc0d3r@gmail.com>
2025-05-27 15:31:35 +0300

    Configure syncpack to work with the workspace protocol
    To learn more, visit https://jamiemason.github.io/syncpack/examples/pnpm-workspace-protocol/

---
+++ syncpack.config.js
@@ -0,0 +1,13 @@
+/** @type {import("syncpack").RcFile} */
+const config = {
+  versionGroups: [
+    {
+      label: "Use workspace protocol for local packages",
+      dependencies: ["$LOCAL"],
+      dependencyTypes: ["dev", "peer", "prod"],
+      pinVersion: "workspace:*",
+    },
+  ],
+};
+
+export default config;
