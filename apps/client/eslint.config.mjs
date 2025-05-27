198e2329938e10e1ad8ea20726bc2c6b0f657267
Mark <m4rc0d3r@gmail.com>
2025-05-27 14:39:00 +0300

    Extend the basic ESLint configuration for use in a client application

---
+++ apps/client/eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import base from "@test-and-be-tested/eslint-config/base";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    extends: base,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: ["vue"],
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+]);
