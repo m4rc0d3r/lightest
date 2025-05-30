e25ced573f4821ebccdb24f93170e6e7b994d040
Mark <m4rc0d3r@gmail.com>
2025-05-31 00:03:47 +0300

    refactor(core): create a package for the core functionality

---
+++ packages/core/eslint.config.js
@@ -0,0 +1,3 @@
+import base from "@test-and-be-tested/eslint-config/base";
+
+export default base;
---
+++ packages/core/lint-staged.config.js
@@ -0,0 +1,12 @@
+import base from "@test-and-be-tested/lint-staged-config/base";
+import { setUpTasksForTypescriptFiles } from "@test-and-be-tested/lint-staged-config/helpers";
+
+export default {
+  ...base,
+  ...setUpTasksForTypescriptFiles([
+    {
+      glob: "src/**/*.{ts,mts,cts,tsx}",
+      pathToConfigFile: "tsconfig.json",
+    },
+  ]),
+};
