fc28365434592d447093c648ed8b3228b091b404
Mark <m4rc0d3r@gmail.com>
2025-06-03 01:46:04 +0300

    feat(client): generate checkbox component using shadcn/vue

--- apps/client/eslint.config.js
+++ apps/client/eslint.config.js
@@ -39,7 +39,7 @@ export default defineConfig([
       "vue/multi-word-component-names": [
         "error",
         {
-          ignores: ["Button", "Input", "Card", "Select"],
+          ignores: ["Button", "Input", "Card", "Select", "Checkbox"],
         },
       ],
       "vue/no-reserved-component-names": [
