68776fc7641a93bfb35d1f75b96e433929b24b39
Mark <m4rc0d3r@gmail.com>
2025-06-03 19:56:10 +0300

    feat(client): create a link component

---
+++ apps/client/src/components/ui/router-link-2/RouterLink2.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import type { RouterLink, RouterLinkProps } from "vue-router";
+
+import { cn } from "@/lib/utils";
+
+type Props = {
+  class?: HTMLAttributes["class"];
+} & RouterLinkProps;
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <RouterLink
+    :class="cn('text-link-foreground underline-offset-4 hover:underline', props.class)"
+    v-bind="props"
+  >
+    <slot />
+  </RouterLink>
+</template>
---
+++ apps/client/src/components/ui/router-link-2/index.ts
@@ -0,0 +1,1 @@
+export { default as RouterLink2 } from "./RouterLink2.vue";
