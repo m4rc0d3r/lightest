bc72859a5257a1f5fb9c9670ff3ccf5f4644e908
Mark <m4rc0d3r@gmail.com>
2025-06-04 15:07:42 +0300

    refactor(client): rewrite test list

--- apps/client/src/components/tests-view/TestItem.vue
+++ apps/client/src/components/tests-view/TestItem.vue
@@ -1,116 +1,59 @@
-<template>
-  <li class="test">
-    <span class="title">{{ test.title }}</span>
-    <span class="number-of-questions">Number of questions: {{ test.numberOfQuestions }}</span>
-    <span
-      class="grade"
-      v-if="testMode === passedTestMode && typeof (test as BriefPassedTest).score === 'number'"
-      >Grade: {{ (test as BriefPassedTest).score.toFixed(1) }} out of {{ test.grade }}</span
-    >
-    <span class="grade" v-else>Grade: {{ test.grade }}</span>
-    <router-link
-      class="open-test-page-button"
-      v-if="testMode === editableTestMode"
-      :to="`/edit-test/${test.id}`"
-    >
-      Edit
-    </router-link>
-    <router-link
-      class="open-test-page-button"
-      v-else-if="testMode === passableTestMode"
-      :to="`/pass-test/${test.id}`"
-    >
-      Pass
-    </router-link>
-    <router-link
-      class="open-test-page-button"
-      v-else-if="testMode === passedTestMode"
-      :to="`/passed-test/${test.id}`"
-    >
-      View result
-    </router-link>
-  </li>
-</template>
-
-<script lang="ts">
-import type { PropType } from "vue";
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
 
 import type { TestMode } from "./shared";
 import { TEST_MODE } from "./shared";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import type { BriefPassedTest, BriefTest } from "@/dtos/test/brief";
 
-export default defineComponent({
-  props: {
-    test: {
-      type: Object as PropType<BriefTest | BriefPassedTest>,
-      required: true,
-    },
-    testMode: {
-      type: String as PropType<TestMode>,
-      required: true,
-    },
-  },
+type Props = {
+  test: BriefTest | BriefPassedTest;
+  testMode: TestMode;
+};
 
-  computed: {
-    editableTestMode() {
-      return TEST_MODE.EDITABLE;
-    },
-
-    passableTestMode() {
-      return TEST_MODE.PASSABLE;
-    },
-
-    passedTestMode() {
-      return TEST_MODE.PASSED;
-    },
-  },
-});
+defineProps<Props>();
 </script>
 
-<style lang="scss" scoped>
-.test {
-  display: flex;
-  flex-direction: column;
-
-  padding: 10px;
-  border: 4px solid #1e434c;
-  border-radius: 5px;
-
-  > * {
-    margin-bottom: 5px;
-  }
-  > *:last-child {
-    margin-bottom: 0;
-  }
-}
-
-.title {
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: #070f11;
-}
-
-.number-of-questions {
-  font-size: 1.2rem;
-  color: #070f11;
-}
-
-.grade {
-  font-size: 1.2rem;
-  color: #070f11;
-}
-
-.open-test-page-button {
-  border: 1px solid black;
-  border-radius: 5px;
-
-  font-size: 1.4rem;
-  color: white;
-  text-align: center;
-  text-decoration: none;
-
-  background-color: #1e434c;
-}
-</style>
+<template>
+  <Card class="gap-2">
+    <CardHeader>
+      <CardTitle class="text-2xl">{{ test.title }}</CardTitle>
+    </CardHeader>
+    <CardContent class="text-lg">
+      <p>Number of questions: {{ test.numberOfQuestions }}</p>
+      <p
+        v-if="testMode === TEST_MODE.PASSED && typeof (test as BriefPassedTest).score === 'number'"
+      >
+        Grade: {{ (test as BriefPassedTest).score.toFixed(1) }} out of {{ test.grade }}
+      </p>
+      <p class="grade" v-else>Grade: {{ test.grade }}</p>
+    </CardContent>
+    <CardFooter class="justify-end">
+      <Button as-child>
+        <RouterLink
+          class="open-test-page-button"
+          v-if="testMode === TEST_MODE.EDITABLE"
+          :to="`/edit-test/${test.id}`"
+        >
+          Edit
+        </RouterLink>
+        <RouterLink
+          class="open-test-page-button"
+          v-else-if="testMode === TEST_MODE.PASSABLE"
+          :to="`/pass-test/${test.id}`"
+        >
+          Pass
+        </RouterLink>
+        <RouterLink
+          class="open-test-page-button"
+          v-else-if="testMode === TEST_MODE.PASSED"
+          :to="`/passed-test/${test.id}`"
+        >
+          View result
+        </RouterLink>
+      </Button>
+    </CardFooter>
+  </Card>
+</template>
--- apps/client/src/components/tests-view/TestList.vue
+++ apps/client/src/components/tests-view/TestList.vue
@@ -1,48 +1,21 @@
-<template>
-  <ol class="tests">
-    <TestItem v-for="test in tests" :key="test.id" :test="test" :test-mode="testMode" />
-  </ol>
-</template>
-
-<script lang="ts">
-import type { PropType } from "vue";
-import { defineComponent } from "vue";
-
+<script setup lang="ts">
 import type { TestMode } from "./shared";
 
 import TestItem from "@/components/tests-view/TestItem.vue";
 import type { BriefPassedTest, BriefTest } from "@/dtos/test/brief";
 
-export default defineComponent({
-  components: {
-    TestItem,
-  },
+type Props = {
+  tests: BriefTest[] | BriefPassedTest[];
+  testMode: TestMode;
+};
 
-  props: {
-    tests: {
-      type: Array as PropType<BriefTest[] | BriefPassedTest[]>,
-      required: true,
-    },
-    testMode: {
-      type: String as PropType<TestMode>,
-      required: true,
-    },
-  },
-});
+defineProps<Props>();
 </script>
 
-<style lang="scss" scoped>
-.tests {
-  display: flex;
-  flex-direction: column;
-  // border: 2px solid black;
-  padding: 10px 0;
-
-  > * {
-    margin-bottom: 10px;
-  }
-  > *:last-child {
-    margin-bottom: 0;
-  }
-}
-</style>
+<template>
+  <ul class="flex flex-col gap-3">
+    <li v-for="test in tests" :key="test.id">
+      <TestItem :test="test" :test-mode="testMode" />
+    </li>
+  </ul>
+</template>
