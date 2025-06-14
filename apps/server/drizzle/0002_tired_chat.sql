98d293454cb0271ca04b9ff29050b47de55dcd4c
Mark <m4rc0d3r@gmail.com>
2025-06-14 22:07:04 +0300

    refactor(server): remove everything related to the old database schema

---
+++ apps/server/drizzle/0002_tired_chat.sql
@@ -0,0 +1,31 @@
+DROP TABLE "answer_option" cascade;
+
+--> statement-breakpoint
+DROP TABLE "correct_extended_answer" cascade;
+
+--> statement-breakpoint
+DROP TABLE "passed_answer_option" cascade;
+
+--> statement-breakpoint
+DROP TABLE "passed_extended_answer" cascade;
+
+--> statement-breakpoint
+DROP TABLE "passed_question" cascade;
+
+--> statement-breakpoint
+DROP TABLE "passed_test" cascade;
+
+--> statement-breakpoint
+DROP TABLE "question" cascade;
+
+--> statement-breakpoint
+DROP TABLE "session" cascade;
+
+--> statement-breakpoint
+DROP TABLE "test" cascade;
+
+--> statement-breakpoint
+DROP TABLE "user" cascade;
+
+--> statement-breakpoint
+DROP TYPE "public"."question_type";
