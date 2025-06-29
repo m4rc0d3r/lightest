d8405c5d7780cfe5ac0636f6dcb96547f1d5e891
Mark <m4rc0d3r@gmail.com>
2025-06-29 20:36:48 +0300

    chore(client): add Tailwind to work with styles

--- apps/client/prettier.config.js
+++ apps/client/prettier.config.js
@@ -3,6 +3,7 @@ import base from "@lightest/prettier-config/base";
 /** @type {import("prettier").Config} */
 export default {
   ...base,
+  plugins: [...(base.plugins ?? []), "prettier-plugin-tailwindcss"],
   overrides: [
     ...(base.overrides ?? []),
     {
