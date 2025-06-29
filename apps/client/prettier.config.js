91915e1bbee62fe102a82d934cddf7978a86427e
Mark <m4rc0d3r@gmail.com>
2025-06-29 15:43:13 +0300

    chore(prettier): migrate client application-specific settings to its own configuration

--- apps/client/prettier.config.js
+++ apps/client/prettier.config.js
@@ -3,4 +3,13 @@ import base from "@lightest/prettier-config/base";
 /** @type {import("prettier").Config} */
 export default {
   ...base,
+  overrides: [
+    ...(base.overrides ?? []),
+    {
+      files: "*.svg",
+      options: {
+        plugins: ["@prettier/plugin-xml"],
+      },
+    },
+  ],
 };
--- packages/prettier-config/src/base.js
+++ packages/prettier-config/src/base.js
@@ -2,14 +2,6 @@
 const config = {
   printWidth: 100,
   plugins: ["prettier-plugin-jsdoc"],
-  overrides: [
-    {
-      files: "*.svg",
-      options: {
-        plugins: ["@prettier/plugin-xml"],
-      },
-    },
-  ],
 };
 
 export default config;
