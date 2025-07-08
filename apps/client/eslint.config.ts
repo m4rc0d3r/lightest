d189ddb06cf2e0126a7cf44bbf6ded6a604c065d
Mark <m4rc0d3r@gmail.com>
2025-07-09 01:43:07 +0300

    feat(client): generate label component using shadcn/vue

--- apps/client/eslint.config.ts
+++ apps/client/eslint.config.ts
@@ -23,7 +23,7 @@ const config: ReturnType<typeof defineConfigWithVueTs> = defineConfigWithVueTs(
       "vue/multi-word-component-names": [
         "error",
         {
-          ignores: ["Avatar", "Badge", "Button", "Card", "Checkbox", "Dialog", "Input"],
+          ignores: ["Avatar", "Badge", "Button", "Card", "Checkbox", "Dialog", "Input", "Label"],
         },
       ],
     },
